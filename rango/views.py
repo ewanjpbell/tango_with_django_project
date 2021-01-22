4ec08734bbcbd24929420b5b9dd01f12d4c9aacc
Ewan Bell <2332670b@student.gla.ac.uk>
2021-01-22 20:40:56 +0000

    chapter 4 commit

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    
-    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About</a>")
+    #Construct a dictionary to pass to the template engine as its context
+    #Note the key boldmessage matches to the template
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    #Return a rendered response to send to the client.
+    #Make use of the shortcurt function to make life easier
+    #The first parameter is the template we wish to use.
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
-
+    context_dict ={'boldmessage': 'This tutorial has been put together by Ewan Bell.'}
+    
+    return render(request, 'rango/about.html', context=context_dict)
